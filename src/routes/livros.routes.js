47ed7123952cec8c62ec269330e3a3718d8918c5
YoranHansel <yoranhansel@gmail.com>
2026-03-18 10:48:37 -0300

    definindo as routes

---
+++ src/routes/livros.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+const livrosCountrouller = require('../controllers/livros.controllers');
+
+router.get('/', livrosCountrouller.listarLivros);
+
+module.export = {router};
